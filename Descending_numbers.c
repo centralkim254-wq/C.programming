fafeaf5ac8889d325eb6b2e1614cbfaed2013a1e
allan <centralkim254@gmail.com>
2025-10-27 10:41:01 +0300

    Add Descending_numbers.c to print numbers 100 to 50
    Implement a program that prints numbers from 100 to 50 in descending order.

---
+++ Descending_numbers.c
@@ -0,0 +1,12 @@
+/*
+Name:Allan kimutai
+Reg:PA106/G/28722/25
+Description: descending numbered 
+#include <stdio.h>
+
+int main() {
+    for (int i =100; i >=50; i--) {
+         printf("%d\n" , i);
+    }
+    return 0;
+}    
